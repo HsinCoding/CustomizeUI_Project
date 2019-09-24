bfe80207d8a24452e614f53a11ce02f751e14762
starlychen <starly.chen@car-plus.com.tw>
2019-09-24 11:25:42 +0800

    The first commit

---
+++ CustomizeUI_Project/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CustomizeUI_Project
+//
+//  Created by ＵＳＥＲ on 2019/9/24.
+//  Copyright © 2019 hsin_project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Hi The First Time!"
+        
+    }
+
+
+}
+
